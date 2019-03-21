f4ea460ff1750476d7d027bdcb0e928af52b9ff8
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-21 19:34:05 -0400

    submit button

--- assets/script/app.js
+++ assets/script/app.js
@@ -12,49 +12,56 @@ firebase.initializeApp(config);
 var database = firebase.database();
 var auth = firebase.auth();
 
-var weight = 75;
-var height = 1.75;
-var bmi = (weight / Math.pow(height, 2)).toFixed(2);
+$("#BMI-Submit").on("click", function() {
+  //BMI calculation
+  var weight = 75; //$("#weight").val();
+  var height = 1.75; //$("#height").val();
+  var bmi = (weight / Math.pow(height, 2)).toFixed(2);
 
-console.log(bmi);
-
-query = "batman";
-queryURL =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
-  query +
-  "&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyCo0muFnSg2iOmivzJSgDYm9CGB3t-_mmw";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  var items = response.items;
-  var youtubeLink = "https://www.youtube.com/embed/";
-
-  for (var i = 0; i < items.length; i++) {
-    videoId = youtubeLink + items[i].id.videoId;
-    videoDescription = items[i].snippet.description;
-    videoThumbnail = items[i].snippet.thumbnails.default.url;
-
-    newDiv =
-      "<div><img class='thumbClick' vId=" +
-      videoId +
-      " src=" +
-      videoThumbnail +
-      ">" +
-      videoDescription +
-      "</div>";
-    $(".container").append(newDiv);
-    $(".thumbClick").on("click", function() {
-      $(".container").empty();
-      var videoLink = $(this).attr("vId");
-
-      $(".container").append(
-        "<iframe width='640' height='390' src=" +
-          videoLink +
-          "?enablejsapi=1&&origin=https://example.com></iframe>"
-      );
-    });
-  }
+  console.log(bmi);
 });
+function showVideos() {
+  query = "batman";
+  queryURL =
+    "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
+    query +
+    "&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyCo0muFnSg2iOmivzJSgDYm9CGB3t-_mmw";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var items = response.items;
+    var youtubeLink = "https://www.youtube.com/embed/";
+
+    for (var i = 0; i < items.length; i++) {
+      //placeholders for video info
+      var videoId = youtubeLink + items[i].id.videoId;
+      var videoDescription = items[i].snippet.description;
+      var videoThumbnail = items[i].snippet.thumbnails.default.url;
+
+      //div that displays thumbnails
+      newDiv =
+        "<div><img class='thumbClick' vId=" +
+        videoId +
+        " src=" +
+        videoThumbnail +
+        ">" +
+        videoDescription +
+        "</div>";
+      $(".container").append(newDiv);
+      $(".thumbClick").on("click", function() {
+        //removes current content
+        $(".container").empty();
+        var videoLink = $(this).attr("vId");
+        //embeds clicked video
+        $(".container").append(
+          "<iframe width='640' height='390' src=" +
+            videoLink +
+            "?enablejsapi=1&&origin=https://example.com></iframe>"
+        );
+      });
+    }
+  });
+}
