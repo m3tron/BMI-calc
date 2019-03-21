64363605868b06bb3e7dfbfa16b9afc87d78f0f4
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-21 16:32:54 -0400

    now plays video on an embeded player

--- assets/script/app.js
+++ assets/script/app.js
@@ -10,15 +10,51 @@ var config = {
 firebase.initializeApp(config);
 
 var database = firebase.database();
+var auth = firebase.auth();
 
+var weight = 75;
+var height = 1.75;
+var bmi = (weight / Math.pow(height, 2)).toFixed(2);
+
+console.log(bmi);
+
+query = "batman";
 queryURL =
-  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=path+of+exile&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyCo0muFnSg2iOmivzJSgDYm9CGB3t-_mmw";
+  "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" +
+  query +
+  "&order=rating&type=video&videoDefinition=high&videoEmbeddable=true&key=AIzaSyCo0muFnSg2iOmivzJSgDYm9CGB3t-_mmw";
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
   console.log(response);
+  var items = response.items;
+  var youtubeLink = "https://www.youtube.com/embed/";
 
-  var videoLink = "https://www.youtube.com/watch?v=";
+  for (var i = 0; i < items.length; i++) {
+    videoId = youtubeLink + items[i].id.videoId;
+    videoDescription = items[i].snippet.description;
+    videoThumbnail = items[i].snippet.thumbnails.default.url;
+
+    newDiv =
+      "<div><img class='thumbClick' vId=" +
+      videoId +
+      " src=" +
+      videoThumbnail +
+      ">" +
+      videoDescription +
+      "</div>";
+    $(".container").append(newDiv);
+    $(".thumbClick").on("click", function() {
+      $(".container").empty();
+      var videoLink = $(this).attr("vId");
+
+      $(".container").append(
+        "<iframe width='640' height='390' src=" +
+          videoLink +
+          "?enablejsapi=1&&origin=https://example.com></iframe>"
+      );
+    });
+  }
 });
