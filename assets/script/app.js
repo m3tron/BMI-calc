54cefac5d58f9e8c24716d329eef1ce7c98a3c1e
Sunny Heyar <sunny.heyar@gmail.com>
2019-03-23 22:57:34 -0400

    fixed parameter for bmi levels

--- assets/script/app.js
+++ assets/script/app.js
@@ -38,13 +38,13 @@ $(document).ready(function() {
         "<p>The Risks of Being Underweight: Being underweight can cause as many health concerns to an individual as being overweight can. Being underweight causes your body to not get nutrients that is desperately needs to build healthy bones, hair etc.<br> Due to this you can face skin or hair problems causing poor dental health or hair loss. Your body can tend to feel tired all of the time due to the lack of calories your body needs to produce energy. A more serious result of being understand is Osteporosis, causing the bones to become so weak they are more prone to breaking.<br>There are a number of treatment plans an individual can take on to overcome being underweight. Adding snacks high in carbohydrates and eating smaller but more meals throughout the day that are calorie dense will begin to slowly repair your body’s nutrient levels and improve your overall health.</p>"
       );
       showVideos("how to gain weight");
-    } else if (18.5 < bmi < 24.9) {
+    } else if (18.5 < bmi && bmi < 24.9) {
       $("#main").empty();
       $("#main").append(
         "<p>How to Maintain a Healthy BMI: Whatever you have been doing so far to maintain a healthy BMI-keep on following that routine. Regular exercises is crucial for the body to burn calories and build muscles which can in turn help burn more calories even when you’re not working out. It is important to be cautious of what you’re eating as not only does your body need a number of calories a day but it needs the appropriate nutrients as well. Ensure you are giving your body 5 servings of fruits and vegetables a day with the nutrients and minerals you need.</p>"
       );
       showVideos("maintain fitness");
-    } else if (25 < bmi < 29.9) {
+    } else if (25 < bmi && bmi < 29.9) {
       $("#main").empty();
       $("#main").append(
         "<p>How To Reduce your BMI to Healthy: Tracking your calories daily can assist you with understanding where your highest calorie dense meals are coming from. It is important you do not drastically reduce your calorie account as your body also needs time to adjust. Removing foods high in carbs and substituting with fruits and vegetables can provide your body with the necessary nutrients but also be light in calories. It is critical you begin working out, after consulting a doctor. Create a workout plan that focuses on cardio and other high intensity workouts that get your heart pumping and causes you to sweat. Consider joining the gym, or a active sport such as swimming or dance. Not only will working out cause your body to burn calories and in turn lose weight you are also helping your heart get stronger. Working out and an overall healthy/active lifestyle will also force you to stay motivated and on track. You have to remember that working out will take time to see progress but you must remember how crucial it is for your body, heart and mind to stay focused.</p>"
@@ -110,16 +110,25 @@ $(document).ready(function() {
   }
 
   function loginUser(email, password) {
-    auth.signInWithEmailAndPassword(email, password).then(alert("teehee"));
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(alert("teehee"))
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(error.code, error.message);
+      });
   }
 
-  $("#signUpBtn").on("click", function() {
+  $("#signUpBtn").on("click", function(e) {
+    e.preventDefault();
     var email = $("#inputEmail").val();
     var password = $("#inputPassword").val();
     signUpNewUser(email, password);
   });
 
-  $("#loginBtn").on("click", function() {
+  $("#loginBtn").on("click", function(e) {
+    e.preventDefault();
     var email = $("#inputLoginEmail").val();
     var password = $("#inputLoginPassword").val();
     loginUser(email, password);
